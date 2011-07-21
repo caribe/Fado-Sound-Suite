e2da4266a3567d86b45c6148a9600659ef3cb6f8
caribe <vbuttazzo@yahoo.com>
2011-07-21 08:37:23 +0200

    Move to GPL3

--- Fadoapp/core/core.cpp
+++ Fadoapp/core/core.cpp
@@ -1,3 +1,25 @@
+/*
+ * Fado Sound Suite - Modular synthetic music generator and sound processor
+ *
+ * Copyright (C) 2011 Vincenzo Buttazzo <vbuttazzo@yahoo.com>
+ *
+ * This file is part of Fado Sound Suite.
+ *
+ * Fado Sound Suite is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fado Sound Suite is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fado Sound Suite.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "core/core.h"
 
 Core::Core(QObject *parent) : QObject(parent)
@@ -123,6 +145,7 @@ void Core::loadPluginsFolder(QDir &dir, QHash<QString, QStandardItem *> &generat
 					effectsBuffer[machine->author]->appendRow(item);
 				}
 
+				item->setToolTip(machine->description);
 				item->setEditable(false);
 				item->setData(id);
 
